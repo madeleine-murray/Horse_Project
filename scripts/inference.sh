9d022599bce64722c8613b25e4c25ca650b213ea
madeleine-murray <47334370+madeleine-murray@users.noreply.github.com>
2020-05-10 21:09:34 +0200

    Script for inferences

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -11,17 +11,17 @@ OMP_NUM_THREADS=1; OPENBLAS_NUM_THREADS=1; #avoid mashup during parallel runs
 while [ $processed -ne $expected ]; do #while the number of processed line is different to the number of expected lines
     
     # infer with sr
-    /disk/regine/data2/madeleine/simulations/messer/sr -D /disk/regine/data2/madeleine/simulations/messer/playground/generateFrac2_6tp/generated_$filename.txt -N $popsize -n $ITERATIONS -g 500 -F 20 -f $PRINT -s 100 -P /disk/regine/data2/madeleine/simulations/schraiber/playground/population/$popsize.pop -e $RANDOM -o /disk/regine/data2/madeleine/simulations/messer/playground/inference_6tp/infered${run}_$filename &>/dev/null
+    /Horse_Projects/scripts/sr -D /Horse_Projects/scripts/slimTemplates/generations/generated_$filename.txt -N $popsize -n $ITERATIONS -g 500 -F 20 -f $PRINT -s 100 -P /Horse_Projects/scripts/demography/$popsize.pop -e $RANDOM -o /Horse_Projects/scripts/results/inferences/infered${run}_$filename &>/dev/null
 
     # check inference has been processed fully (bugs rarely happen but best to avoid)
-    gunzip -f /disk/regine/data2/madeleine/simulations/messer/playground/inference_6tp/infered${run}_$filename.param.gz #unzip interesting file (overwriting previous file)
-    line=`wc -l /disk/regine/data2/madeleine/simulations/messer/playground/inference_6tp/infered${run}_$filename.param | cut -d" " -f1` #use the number of line in file as the number of iterations processed
+    gunzip -f /Horse_Projects/scripts/results/inferences/infered${run}_$filename.param.gz #unzip interesting file (overwriting previous file)
+    line=`wc -l /Horse_Projects/scripts/results/inferences/infered${run}_$filename.param | cut -d" " -f1` #use the number of line in file as the number of iterations processed
     processed=`echo "$line - 1" | bc` #leave the header out to count the number of processed lines
 
 done
 
-rm /disk/regine/data2/madeleine/simulations/messer/playground/inference_6tp/infered${run}_$filename.time.gz
-rm /disk/regine/data2/madeleine/simulations/messer/playground/inference_6tp/infered${run}_$filename.traj.gz
+rm /Horse_Projects/scripts/results/infences/infered${run}_$filename.time.gz
+rm /Horse_Projects/scripts/results/inferences/infered${run}_$filename.traj.gz
 
 # check that no output file exists                                                                                                                                                                      
 if [ -f /tmp/$filename.err ]; then #if such file exists                                                                                                                                                 
